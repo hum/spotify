0d518de2f12dfc5d04db26f8d04ba7c1b07558ca
hum <matonohamartin@gmail.com>
2021-04-14 09:44:20 +0200

    format tests

--- client.test.ts
+++ client.test.ts
@@ -1,32 +1,36 @@
-import { assertEquals, assertThrows, assertThrowsAsync } from "https://deno.land/std@0.93.0/testing/asserts.ts";
+import {
+  assertEquals,
+  assertThrows,
+  assertThrowsAsync,
+} from "https://deno.land/std@0.93.0/testing/asserts.ts";
 import { Client } from "./client.ts";
 
 const TOKEN = Deno.env.get("spotify_access_token") ?? "";
 
 Deno.test("Incorrect token provided error", async () => {
-    const spotify = new Client({
-        accessToken: "aaa"
-    });
+  const spotify = new Client({
+    accessToken: "aaa",
+  });
 
-    await assertThrowsAsync(() => spotify.getArtist("joji"));
-})
+  await assertThrowsAsync(() => spotify.getArtist("joji"));
+});
 
 Deno.test("Fetch artist data", async () => {
-    const spotify = new Client({
-        accessToken: TOKEN,
-    });
+  const spotify = new Client({
+    accessToken: TOKEN,
+  });
 
-    const artist = await spotify.getArtist("joji");
-    const expected = {
-        name: "Joji",
-        id: "3MZsBdqDrRTJihTHQrO6Dq",
-        genres: ["alternative r&b", "viral pop"],
-        type: "artist",
-    }
-    assertEquals(expected.name, artist.name);
-    assertEquals(expected.id, artist.id);
-    assertEquals(expected.genres, artist.genres);
-    assertEquals(expected.type, artist.type);
+  const artist = await spotify.getArtist("joji");
+  const expected = {
+    name: "Joji",
+    id: "3MZsBdqDrRTJihTHQrO6Dq",
+    genres: ["alternative r&b", "viral pop"],
+    type: "artist",
+  };
+  assertEquals(expected.name, artist.name);
+  assertEquals(expected.id, artist.id);
+  assertEquals(expected.genres, artist.genres);
+  assertEquals(expected.type, artist.type);
 
-    assertThrowsAsync(() => spotify.getArtist(""), Error);
-})
+  assertThrowsAsync(() => spotify.getArtist(""), Error);
+});
