679489d947696c081f551742c62b07126d54216c
hum <matonohamartin@gmail.com>
2021-04-14 16:55:31 +0200

    move caller to its own handler

--- client.ts
+++ client.ts
@@ -1,50 +1,23 @@
-import { ArtistObj } from "./models.ts";
+import { ArtistObj } from "./types.ts";
 import { endpoints, SearchType } from "./endpoints.ts";
-
-export interface ClientConfig {
-  accessToken: string;
-  refreshToken?: string;
-  /**
-     * Base URL for the Spotify API
-     * @default "https://api.spotify.com/v1"
-     */
-  baseUrl?: string;
-}
+import { Artist } from "./models/models.ts";
+import { Caller, CallerOpt } from "./handlers/caller.ts";
 
 export class Client {
-  #conf: ClientConfig;
+  #caller: Caller;
 
-  constructor(conf: ClientConfig) {
-    conf.baseUrl = conf.baseUrl ?? "https://api.spotify.com/v1";
-    this.#conf = conf;
+  constructor(conf: CallerOpt) {
+    this.#caller = new Caller(conf);
   }
 
-  async getArtist(name: string): Promise<ArtistObj> {
+  async getArtist(name: string): Promise<Artist> {
     if (name.length == 0) {
       throw new Error("Parameter 'name' needs to be specified.");
     }
-    const result = await this.fetch(
+    const result = await this.#caller.fetch(
       endpoints.SEARCH(name, SearchType.Artist, "US"),
     );
-    return result["artists"]["items"][0] as ArtistObj;
-  }
-
-  // deno-lint-ignore no-explicit-any
-  private async fetch(url: string): Promise<any> {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Authorization": this.#conf.accessToken,
-      },
-    });
-    const json = await response.json();
-
-    if (response.status != 200) {
-      const err = `The API returned a non-successful response. ${
-        JSON.stringify(json["error"])
-      }.`;
-      throw new Error(err);
-    }
-    return json;
+    const data = result["artists"]["items"][0] as ArtistObj;
+    return new Artist(data, this.#caller);
   }
 }
---
+++ handlers/caller.ts
@@ -0,0 +1,32 @@
+export interface CallerOpt {
+  accessToken: string;
+  refreshToken?: string;
+}
+
+export class Caller {
+  #accessToken: string;
+  #refreshToken: string;
+
+  constructor(conf: CallerOpt) {
+    this.#accessToken = conf.accessToken;
+    this.#refreshToken = conf.refreshToken ?? "";
+  }
+
+  // deno-lint-ignore no-explicit-any
+  async fetch(url: string): Promise<any> {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Authorization": this.#accessToken,
+      },
+    });
+    const json = await response.json();
+
+    if (response.status != 200) {
+      const err = `Server returned a non-successful response. 
+                ${JSON.stringify(json["error"])}`;
+      throw new Error(err);
+    }
+    return json;
+  }
+}
