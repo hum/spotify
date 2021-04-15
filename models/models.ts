0fcb3478ef58694fdb98d6b8fe338e9d231ff63d
hum <matonohamartin@gmail.com>
2021-04-15 16:01:45 +0200

    restructure exports

--- models/models.ts
+++ models/models.ts
@@ -1,4 +1,6 @@
-export { Artist } from "./artist.ts";
-export { Album } from "./album.ts";
 export { SimplifiedArtist } from "./simplifiedArtist.ts";
 export { SimplifiedAlbum } from "./simplifiedAlbum.ts";
+export { SimplifiedTrack } from "./simplifiedTrack.ts";
+export { Artist } from "./artist.ts";
+export { Album } from "./album.ts";
+export { Track } from "./track.ts";
