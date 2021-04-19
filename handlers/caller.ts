7bbc9d1a74a3ca06b7c01e037de92f9fc9163938
hum <matonohamartin@gmail.com>
2021-04-19 10:45:59 +0200

    export caller object

--- handlers/caller.ts
+++ handlers/caller.ts
@@ -1,19 +1,22 @@
 export interface CallerOpt {
-  accessToken: string;
+  accessToken?: string;
   refreshToken?: string;
 }
 
 export class Caller {
   #accessToken: string;
   #refreshToken: string;
+  #count: number;
 
   constructor(conf: CallerOpt) {
-    this.#accessToken = conf.accessToken;
+    this.#accessToken = conf.accessToken ?? "";
     this.#refreshToken = conf.refreshToken ?? "";
+    this.#count = 0;
   }
 
   // deno-lint-ignore no-explicit-any
   async fetch(url: string): Promise<any> {
+    console.log(++this.#count);
     const response = await fetch(url, {
       method: "GET",
       headers: {
@@ -31,4 +34,14 @@ export class Caller {
     }
     return json;
   }
+
+  setCallerOpt(opt: CallerOpt) {
+    if (!this.#accessToken) {
+      this.#accessToken = opt.accessToken ?? "";
+    }
+    this.#refreshToken = opt.refreshToken ?? "";
+  }
 }
+
+const caller = new Caller({});
+export { caller }
