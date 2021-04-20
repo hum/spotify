707316beacf6f7a28caf8d07d4424e1f271df0c2
hum <matonohamartin@gmail.com>
2021-04-20 14:25:04 +0200

    refac endpoints

--- endpoints/endpoints.ts
+++ endpoints/endpoints.ts
@@ -9,35 +9,23 @@ const GET_MULTIPLE_ALBUMS = (opts: opts.MultipleAlbumsOpt) => {
       `Parameter 'ids' did not provide an array between the size of 1 and 20.`,
     );
   }
-
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/albums`;
-
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/albums`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_ALBUM = (opts: opts.AlbumOpt) => {
-  let query = `${API_PREFIX}/albums/${opts.id}`;
-  const params: Record<string, string> = parseOpts(opts);
-
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/albums/${opts.id}`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_ALBUM_TRACKS = (opts: opts.AlbumTracksOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/albums/${opts.id}/tracks`;
-
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/albums/${opts.id}/tracks`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_MULTIPLE_ARTISTS = (opts: opts.MultipleArtists) => {
-  let query = `${API_PREFIX}/artists`;
-  const params: Record<string, string> = parseOpts(opts);
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/artists`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_ARTIST = (opts: opts.ArtistOpt) => {
@@ -45,10 +33,8 @@ const GET_ARTIST = (opts: opts.ArtistOpt) => {
 };
 
 const GET_ARTIST_TOP_TRACKS = (opts: opts.ArtistTopTracksOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/artists/${opts.id}/top-tracks`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/artists/${opts.id}/top-tracks`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_RELATED_ARTISTS = (opts: opts.RelatedArtistsOpt) => {
@@ -56,77 +42,50 @@ const GET_RELATED_ARTISTS = (opts: opts.RelatedArtistsOpt) => {
 };
 
 const GET_ARTISTS_ALBUMS = (opts: opts.ArtistAlbumsOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/artists/${opts.id}/albums`;
-
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/artists/${opts.id}/albums`;
+  return format(query, parseOpts(opts));
 };
 
 const SEARCH = (opts: opts.SearchOpt) => {
+  const query = `${API_PREFIX}/search`;
   opts.q = opts.q.replace(" ", "%20");
 
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/search`;
-
-  query = format(query, params);
-  return query;
+  return format(query, parseOpts(opts));
 };
 
 const GET_ALL_NEW_RELEASES = (opts: opts.NewReleasesOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/browse/new-releases`;
-
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/browse/new-releases`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_ALL_FEATURED_PLAYLISTS = (opts: opts.AllFeaturedPlaylistsOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/browse/featured-playlists`;
-
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/browse/featured-playlists`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_PLAYLIST_ITEMS = (opts: opts.PlaylistItemsOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/playlists/${opts.id}/tracks`;
-
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/playlists/${opts.id}/tracks`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_ALL_CATEGORIES = (opts: opts.AllCategoriesOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/browse/categories`;
-
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/browse/categories`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_CATEGORY = (opts: opts.CategoryOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/browse/categories/${opts.id}`;
-
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/browse/categories/${opts.id}`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_CATEGORY_PLAYLISTS = (opts: opts.CategoryPlaylistsOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/browse/categories/${opts.id}/playlists`;
-
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/browse/categories/${opts.id}/playlists`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_RECOMMENDATIONS = (opts: opts.RecommendationsOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-
-  let query = `${API_PREFIX}/recommendations`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/recommendations`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_RECOMMENDATION_GENRES = () => {
@@ -135,79 +94,60 @@ const GET_RECOMMENDATION_GENRES = () => {
 };
 
 const GET_MULTIPLE_EPISODES = (opts: opts.MultipleEpisodesOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/episodes`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/episodes`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_EPISODE = (opts: opts.EpisodeOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/episodes/${opts.id}`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/episodes/${opts.id}`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_AVAILABLE_MARKETS = () => {
-  return `${API_PREFIX}/markets`;
+  const query = `${API_PREFIX}/markets`;
+  return query;
 };
 
 const GET_MULTIPLE_SHOWS = (opts: opts.MultipleShowsOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/shows`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/shows`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_SHOW = (opts: opts.ShowOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/shows/${opts.id}`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/shows/${opts.id}`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_SHOW_EPISODES = (opts: opts.ShowEpisodesOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/shows/${opts.id}/episodes`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/shows/${opts.id}/episodes`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_SEVERAL_TRACKS = (opts: opts.SeveralTracksOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/tracks`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/tracks`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_TRACK = (opts: opts.TrackOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/tracks/${opts.id}`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/tracks/${opts.id}`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_AUDIO_FEATURES_FOR_SEVERAL_TRACKS = (
   opts: opts.AudioFeaturesForSeveralTracksOpt,
 ) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/audio-features`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/audio-features`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_AUDIO_FEATURES_FOR_TRACK = (opts: opts.AudioFeaturesForTrackOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/audio-features/${opts.id}`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/audio-features/${opts.id}`;
+  return format(query, parseOpts(opts));
 };
 
 const GET_AUDIO_ANALYSIS_FOR_TRACK = (opts: opts.AudioAnalysisForTrackOpt) => {
-  const params: Record<string, string> = parseOpts(opts);
-  let query = `${API_PREFIX}/audio-analysis/${opts.id}`;
-  query = format(query, params);
-  return query;
+  const query = `${API_PREFIX}/audio-analysis/${opts.id}`;
+  return format(query, parseOpts(opts));
 };
 
 export const endpoints = {
