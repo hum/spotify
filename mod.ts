776a3fff239c13ed6d1616c42efa5fd12bbbfbd4
hum <matonohamartin@gmail.com>
2021-04-22 10:18:39 +0200

    expose opts, structs and repeat state

--- mod.ts
+++ mod.ts
@@ -1,2 +1,5 @@
 export { Client } from "./client.ts";
 export * from "./types/types.ts";
+export * from "./opts/opts.ts";
+export * from "./structures/structs.ts";
+export { RepeatState } from "./types/playback.ts";
