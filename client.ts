3e150e6f57174a5798e552c46a9b56a6918511c9
hum <matonohamartin@gmail.com>
2021-04-23 11:36:31 +0200

    add docs

--- client.ts
+++ client.ts
@@ -36,6 +36,12 @@ export class Client {
     caller.setCallerOpt(opt);
   }
 
+  /**
+   * Get information for a single artist
+   * @param name Name of the artist
+   * @param market Optional 2 letter country code
+   * @returns Artist
+   */
   async getArtist(name: string, market?: string): Promise<Artist> {
     if (!name) {
       throw new Error("Parameter 'name' needs to be specified");
@@ -48,6 +54,12 @@ export class Client {
     return new Artist(artist[0]);
   }
 
+  /**
+   * Get information for a single artist.
+   * @param opts Parameters passed to the endpoint
+   * @example opts = {id: '123'}
+   * @returns Artist
+   */
   async getArtistById(opts: opts.ArtistOpt): Promise<Artist> {
     if (opts.id.length == 0) {
       throw new Error("Parameter 'id' needs to be specified");
@@ -61,11 +73,22 @@ export class Client {
     return artist;
   }
 
+  /**
+   * Get information for a single album.
+   * @param name Name of the album
+   * @returns Album
+   */
   async getAlbum(name: string): Promise<Album> {
     const id = await this.getAlbumId(name);
     return await this.getAlbumById({ id: id });
   }
 
+  /**
+   * Get information for a single album.
+   * @param opts Parameters passed to the endpoint
+   * @example opts = {id: '123'}
+   * @returns Album
+   */
   async getAlbumById(opts: opts.AlbumOpt): Promise<Album> {
     if (opts.id.length == 0) {
       throw new Error("Parameter 'id' needs to be specified.");
@@ -77,6 +100,13 @@ export class Client {
     return album;
   }
 
+  /**
+   * Get catalog information for multiple albums.
+   * @param albums Array of Spotify IDs of the albums.
+   * @param market Optional 2 letter country code
+   * @returns Array 
+   * @throws Error 'albums' not specified.
+   */
   async getMultipleAlbums(
     albums: Array<string>,
     market?: string,
@@ -126,6 +156,11 @@ export class Client {
     return value.id;
   }
 
+  /**
+   * Get catalog information for several artists.
+   * @param artists Array of Spotify IDs for the given artists.
+   * @returns Array
+   */
   async getMultipleArtists(artists: Array<string>): Promise<Array<Artist>> {
     const result: Array<Artist> = [];
     for (const artist of artists) {
@@ -135,6 +170,12 @@ export class Client {
     return result;
   }
 
+  /**
+   * Get a list of new album releases featured in Spotify
+   * (shown, for example, on a Spotify player's "Browse" tab)
+   * @param opts Parameters passed to the endpoint
+   * @returns Array
+   */
   async getNewReleases(
     opts?: opts.NewReleasesOpt,
   ): Promise<Array<SimplifiedAlbum>> {
@@ -152,6 +193,12 @@ export class Client {
     return result;
   }
 
+  /**
+   * Get a list of Spotify featured playlists
+   * (shown, for example, on a Spotify player's "Browse" tab)
+   * @param opts Parameters passed to the endpoint
+   * @returns Array
+   */
   async getFeaturedPlaylists(
     opts?: opts.AllFeaturedPlaylistsOpt,
   ): Promise<Array<SimplifiedPlaylist>> {
@@ -169,7 +216,13 @@ export class Client {
     return result;
   }
 
-  async getCategories(opts: opts.AllCategoriesOpt): Promise<Array<Category>> {
+  /**
+   * Get a list of categories used to tag items in Spotify
+   * (on, for example, the Spotify player's "Browse" tab).
+   * @param opts Parameters passed to the endpoint
+   * @returns Array
+   */
+  async getCategories(opts?: opts.AllCategoriesOpt): Promise<Array<Category>> {
     const data = await caller.fetch({
       url: endpoints.GET_ALL_CATEGORIES(opts),
     });
@@ -183,6 +236,13 @@ export class Client {
     return result;
   }
 
+  /**
+   * Get a single category used to tag items in Spotify
+   * (on, for example, the Spotify player's "Browse" tab).
+   * @param opts Parameters passed to the endpoint
+   * @example opts = {categoryId: '123'}
+   * @returns Category
+   */
   async getCategory(opts: opts.CategoryOpt): Promise<Category> {
     const data: CategoryObj = await caller.fetch({
       url: endpoints.GET_CATEGORY(opts),
@@ -191,6 +251,11 @@ export class Client {
     return category;
   }
 
+  /**
+   * Recommendations are generated based on the available information.
+   * @param opts - Parameters passed to the endpoint
+   * @returns RecommendationsObj 
+   */
   async getRecommendations(
     opts: opts.RecommendationsOpt,
   ): Promise<RecommendationsObj> {
@@ -200,6 +265,10 @@ export class Client {
     return data;
   }
 
+  /**
+   * Retrieve a list of available genres.
+   * @returns Array
+   */
   async getRecommendationGenres(): Promise<Array<string>> {
     const data = await caller.fetch({
       url: endpoints.GET_RECOMMENDATION_GENRES(),
@@ -207,6 +276,12 @@ export class Client {
     return data["genres"];
   }
 
+  /**
+   * Get information for several episodes
+   * @param opts Parameter passed to the endpoint
+   * @example opts = {ids: ['123', '456']}
+   * @returns Array
+   */
   async getMultipleEpisodes(
     opts: opts.MultipleEpisodesOpt,
   ): Promise<Array<Episode>> {
@@ -237,12 +312,23 @@ export class Client {
     const result = data["episodes"]["items"][0];
     return result;
   }
-
+  /**
+   * Get information for a single episode.
+   * @param name Name of the episode
+   * @param market Optional 2 letter country code 
+   * @returns SimplifiedEpisode
+   */
   async getEpisode(name: string, market?: string): Promise<SimplifiedEpisode> {
     const data: SimplifiedEpisodeObj = await this.getEpisodeId(name, market);
     return new SimplifiedEpisode(data);
   }
 
+  /**
+   * Get information for a single episode.
+   * @param opts Params passed to the endpoint
+   * @example opts = {id: '123'}
+   * @returns Episode
+   */
   async getEpisodeById(opts: opts.EpisodeOpt): Promise<Episode> {
     const data = await caller.fetch({
       url: endpoints.GET_EPISODE(opts),
@@ -250,6 +336,10 @@ export class Client {
     return new Episode(data);
   }
 
+  /**
+   * Get the list of markets where Spotify is available.
+   * @returns Array
+   */
   async getAvailableMarkets(): Promise<Array<string>> {
     const data = await caller.fetch({
       url: endpoints.GET_AVAILABLE_MARKETS(),
@@ -257,6 +347,12 @@ export class Client {
     return data["markets"] as Array<string>;
   }
 
+  /**
+   * Get information for several shows.
+   * @param opts Parameters passed to the endpoint
+   * @example opts = {id: '123'}
+   * @returns Array
+   */
   async getMultipleShows(
     opts: opts.MultipleShowsOpt,
   ): Promise<Array<SimplifiedShow>> {
@@ -270,6 +366,12 @@ export class Client {
     return result;
   }
 
+  /**
+   * Get Spotify catalog information for a single show.
+   * @param name Name of the show
+   * @param market Optional 2 letter country code
+   * @returns Show 
+   */
   async getShow(name: string, market?: string): Promise<Show> {
     const data: Array<SimplifiedShowObj> = await this.rawSearch({
       q: name,
@@ -280,6 +382,12 @@ export class Client {
     return simpleShow.getAllData();
   }
 
+  /**
+   * Get Spotify catalog information for a single show.
+   * @param opts Parameters passed to the endpoint
+   * @example opts = {id: '123'} 
+   * @returns Show
+   */
   async getShowById(opts: opts.ShowOpt): Promise<Show> {
     const data = await caller.fetch({
       url: endpoints.GET_SHOW(opts),
@@ -287,6 +395,12 @@ export class Client {
     return new Show(data);
   }
 
+  /**
+   * Get Spotify catalog information for multiple tracks.
+   * @param opts - Params passed to the endpoint
+   * @example opts = {ids: ['123', '456']}
+   * @returns Array 
+   */
   async getSeveralTracks(opts: opts.SeveralTracksOpt): Promise<Array<Track>> {
     const data = await caller.fetch({
       url: endpoints.GET_SEVERAL_TRACKS(opts),
@@ -299,6 +413,12 @@ export class Client {
     return result;
   }
 
+  /**
+   * Get Spotify catalog information for a single track.
+   * @param name Name of the track
+   * @param market - Optional 2 letter country code
+   * @returns Track
+   */
   async getTrack(name: string, market?: string): Promise<Track> {
     const data: Array<TrackObj> = await this.rawSearch({
       q: name,
@@ -308,11 +428,24 @@ export class Client {
     return new Track(data[0]);
   }
 
+  /**
+   * Get Spotify catalog information for a single track.
+   * @param opts Parameters passed to the endpoint
+   * @example opts = {id: '123'}
+   * @example opts = {id: '12345', market: "US"}
+   * @returns Track
+   */
   async getTrackById(opts: opts.TrackOpt): Promise<Track> {
     const data = await caller.fetch({ url: endpoints.GET_TRACK(opts) });
     return new Track(data);
   }
 
+  /**
+   * Get audio features for multiple tracks.
+   * @param opts Parameters passed to the endpoint.
+   * @example opts = {ids: ['1', '2']}
+   * @returns Array<AudioFeatures>
+   */
   async getAudioFeaturesForSeveralTracks(
     opts: opts.AudioFeaturesForSeveralTracksOpt,
   ): Promise<Array<AudioFeatures>> {
@@ -327,6 +460,12 @@ export class Client {
     return result;
   }
 
+  /**
+   * Get audio feature information for a single track.
+   * @param opts Parameters passed to the endpoint
+   * @example opts = {id: '123'}
+   * @returns AudioFeatures
+   */
   async getAudioFeaturesForTrack(
     opts: opts.AudioFeaturesForTrackOpt,
   ): Promise<AudioFeatures> {
@@ -336,6 +475,11 @@ export class Client {
     return new AudioFeatures(data);
   }
 
+  /**
+   * Get a detailed audio analysis for a single track.
+   * @param id Spotify ID of the track
+   * @returns AudioAnalysisObj
+   */
   async getAudioAnalysis(id: string): Promise<AudioAnalysisObj> {
     const data = await caller.fetch({
       url: endpoints.GET_AUDIO_ANALYSIS_FOR_TRACK({ id: id }),
@@ -343,12 +487,17 @@ export class Client {
     return data;
   }
 
+  /**
+   * Get an instance of the Player API client.
+   * @returns Player
+   */
   getPlayer(): Player {
     return new Player();
   }
 
   /**
-   * Fetch information about the signed in user.
+   * Get detailed profile information about the current user 
+   * (including the current user's username).
    * @returns PrivateUser
    */
   async getCurrentUser(): Promise<PrivateUser> {
@@ -360,8 +509,8 @@ export class Client {
   }
 
   /**
-   * Fetch information about any Spotify user.
-   * @param userId - ID of the specific account
+   * Get public profile information about a Spotify user.
+   * @param userId - Spotify ID of the specific user.
    * @returns PublicUser
    */
   async getUser(userId: string): Promise<PublicUser> {
@@ -373,8 +522,13 @@ export class Client {
     return new PublicUser(data);
   }
 
-  // TODO:
-  // Expose raw endpoints
+  /**
+   * Get information about albums, artists, playlists, tracks, shows or episodes
+   * that match a keyword string 
+   * @param opts Parameters passed to the function
+   * @example opts = {q: "aaa", type: SearchType.ARTIST}
+   * @returns Array 
+   */
   async rawSearch(
     opts: opts.SearchOpt,
     // deno-lint-ignore no-explicit-any
